332ef7c7b3ade0316575b9b2508d2fe3ca7d2b4f
Okikiola9 <azokiki1996@gmail.com>
2023-06-20 20:25:35 -0700

    ggh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - print just alphabet,
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
