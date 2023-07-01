ca8a53381f301a4c11433e17567eb78afda28c50
Okikiola9 <azokiki1996@gmail.com>
2023-06-30 21:44:38 -0700

    thank you

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * line of code
+	 * Remember:
+	 * - not allowed to use a
+	 * - not allowed to modify p
+	 * - just a statement
+	 * - not allowed to code execp than this line of code
+	 */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * line of code
+   * Remember:
+   * - not allowed to use a
+   * - not allowed to modify p
+   * - just one statement
+   * - not allowed to code except this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
